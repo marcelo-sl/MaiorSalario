c91be94c7052f5e5c0bfdc6a8bd2007ad8458f5b
marcelo-sl <mar.sousa2061@gmail.com>
2020-07-21 19:40:44 -0300

    Initial commit

---
+++ MaiorSalario/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿namespace MaiorSalario
+{
+    class Funcionario
+    {
+        public string nome;
+        public int idade;
+        public double salario;
+
+        public string Apresentacao()
+        {
+            string r = $"{this.nome} com {this.idade} anos e salário de R$ {this.salario:F2} reais.";
+            return r;
+        }
+    }
+}
---
+++ MaiorSalario/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace MaiorSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com a quantidade total de funcionários: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            Funcionario maiorSalario = new Funcionario();
+            maiorSalario.salario = 0.0;
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Funcionario f = new Funcionario();
+
+                Console.WriteLine("Informe o nome do funcionário: ");
+                f.nome = Console.ReadLine();
+
+                Console.WriteLine("Informe a idade do funcionário: ");
+                f.idade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe o salário do funcionário: ");
+                f.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (f.salario > maiorSalario.salario)
+                {
+                    maiorSalario = f;
+                }
+            }
+
+            Console.WriteLine("Maior salário: {0}", maiorSalario.Apresentacao());
+        }
+    }
+}
